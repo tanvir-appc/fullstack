8f58cbd999f64a266dcf43f5fd632cfd2793d722
Tanvir Ahmed Chowdhury <tanvirbangladeshi@gmail.com>
2021-10-05 15:45:53 +0600

    refine credit card validator function

--- creditCardValidator.js
+++ creditCardValidator.js
@@ -20,28 +20,31 @@ const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
+const batch = [
+  valid1, valid2, valid3, valid4, valid5,
+  invalid1, invalid2, invalid3, invalid4, invalid5,
+  mystery1, mystery2, mystery3, mystery4, mystery5
+  ];
 
 
-// Add your functions below:
 const validateCred = arr => {
-  let tempArr = arr
-  console.log(tempArr)
+  let tempArr = arr.slice()
   for(let i=arr.length-2;i>-1;i-=2){
     tempArr[i] *= 2
     if(tempArr[i] > 9){
       tempArr[i] -= 9
     }
   }
-  console.log(tempArr)
   
   let sum = tempArr.reduce((ac,curr) => ac+curr)
-  console.log(sum)
   if(sum%10 === 0){
     return true
-  }else{
-    return false
   }
+  return false
 }
 
-console.log(validateCred(invalid4))
+const findInvalidCards = arr => {
+  return arr.filter(card => validateCred(card))
+}
+
+console.log(findInvalidCards(batch))
